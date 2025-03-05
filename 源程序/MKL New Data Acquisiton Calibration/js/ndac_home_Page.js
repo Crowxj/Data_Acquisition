668024cb9fea8e7861ef5a6e69c2a8e5aa44c0c5
“Crow” <crow_wxj@163.com>
2025-03-05 16:23:42 +0800

    update setup page

--- 源程序/MKL New Data Acquisiton Calibration/js/ndac_home_Page.js
+++ 源程序/MKL New Data Acquisiton Calibration/js/ndac_home_Page.js
@@ -5,6 +5,7 @@
 * 创建时间:2025/03/05 14:01:03
 */
 document.addEventListener('DOMContentLoaded', () => {
+    var SETUPSIGN = false;//退出标识
 
 })
 /**
@@ -36,10 +37,21 @@ ndac_home_Page_min.addEventListener('click', () => {
 const ndac_home_Page_setup = document.getElementById('ndac_home_Page_setup')
 ndac_home_Page_setup.addEventListener('click', () => {
     const ndac_setup_home_Page = document.getElementById('ndac_setup_home_Page')
-    // ndac_setup_home_Page.style.display = 'block';
     ndac_setup_home_Page.style.transition = 'none';
     ndac_setup_home_Page.style.right = '-355px';
     void ndac_setup_home_Page.offsetWidth;
     ndac_setup_home_Page.style.transition = 'right 0.3s ease-in-out';
     ndac_setup_home_Page.style.right = '0';
+})
+/**
+* 模块名:
+* 代码描述:系统设置退出
+* 作者:Crow
+* 创建时间:2025/03/05 16:16:50
+*/
+const ndac_setup_home_Page_exit = document.getElementById('ndac_setup_home_Page_exit')
+ndac_setup_home_Page_exit.addEventListener('click', () => {
+    const ndac_setup_home_Page = document.getElementById('ndac_setup_home_Page')
+    ndac_setup_home_Page.style.transition = 'right 0.3s ease-in-out';
+    ndac_setup_home_Page.style.right = '-355px';
 })
